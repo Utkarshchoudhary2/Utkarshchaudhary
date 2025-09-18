9b183d64b7ad8cb2304b506f9b11928627416da4
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-18 15:38:27 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const hamburger = document.querySelector(".hamburger");
-  const sidebar = document.querySelector(".sidebar");
-  if (hamburger && sidebar) {
-    hamburger.addEventListener("click", () => {
-      sidebar.classList.toggle("active");
-    });
-  }
-});
