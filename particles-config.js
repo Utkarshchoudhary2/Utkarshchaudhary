df1aa0be86cf1cf8a366a119f11ac827b5b81a0d
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-18 21:39:16 +0530

    Update particles-config.js

--- particles-config.js
+++ particles-config.js
@@ -1,1 +1,42 @@
 
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 100 },
+    "color": { "value": "#ffffff" },
+    "shape": { "type": "circle" },
+    "opacity": {
+      "value": 0.5,
+      "random": false
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "grab" },
+      "onclick": { "enable": true, "mode": "push" }
+    },
+    "modes": {
+      "grab": { "distance": 200, "line_linked": { "opacity": 0.7 } },
+      "push": { "particles_nb": 4 }
+    }
+  },
+  "retina_detect": true
+});
