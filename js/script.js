54c93176f3d2d1b0ca7a4730f9ba0ec401a7bf23
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-19 18:42:46 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("expanded");
-}
-particlesJS.load('particles-js', 'js/particles.json', function() {
-  console.log('particles.js loaded');
-});
