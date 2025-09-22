d03a8f8e26915b812991dbd32ea989036ef20163
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-22 17:02:00 +0530

    Update auth.js

--- assets/auth.js
+++ assets/auth.js
@@ -1,1 +1,17 @@
+const auth = firebase.auth();
+const protectedPages = [
+  "index.html","about.html","projects.html",
+  "resume.html","contact.html","game.html"
+];
+
+auth.onAuthStateChanged(user => {
+  const page = location.pathname.split("/").pop();
+  if (!user && protectedPages.includes(page)) {
+    location.href = "login.html";
+  }
+});
+
+function logout() {
+  auth.signOut().then(()=>location.href="login.html");
+}
 
