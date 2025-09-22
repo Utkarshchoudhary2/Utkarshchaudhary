1dbecfe9e943e853a50089dae37b6d6c15266f4e
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-22 16:59:14 +0530

    Create firebase-config.js

---
+++ assets/js /firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1NtsmihmojQsWzOwqRo-286cv12spgdE",
+  authDomain: "utkarshchaudhary-portfolio.firebaseapp.com",
+  projectId: "utkarshchaudhary-portfolio",
+  storageBucket: "utkarshchaudhary-portfolio.firebasestorage.app",
+  messagingSenderId: "487685457505",
+  appId: "1:487685457505:web:797c7ac6cc2e40d8c1cc20",
+  measurementId: "G-VYF9PRY0F9"
+};
+firebase.initializeApp(firebaseConfig);
