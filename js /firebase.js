7b72bdba67bfbdbba284ce8ab7734371d90453bf
Utkarsh chaudhary <155638059+Utkarshchoudhary2@users.noreply.github.com>
2025-09-22 17:41:23 +0530

    Create firebase.js

---
+++ js /firebase.js
@@ -0,0 +1,13 @@
+// js/firebase.js
+// Replace with your actual Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB1NtsmihmojQsWzOwqRo-286cv12spgdE",
+  authDomain: "utkarshchaudhary-portfolio.firebaseapp.com",
+  projectId: "utkarshchaudhary-portfolio",
+  storageBucket: "utkarshchaudhary-portfolio.appspot.com",
+  messagingSenderId: "487685457505",
+  appId: "1:487685457505:web:797c7ac6cc2e40d8c1cc20",
+  measurementId: "G-VYF9PRY0F9"
+};
+
+firebase.initializeApp(firebaseConfig);
